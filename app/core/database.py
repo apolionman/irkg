e620cf608434b571fb293a347a2c951a3ed7fd33
Lawrence <claudiuslawrence@gmail.com>
2025-03-07 09:14:52 +0400

    change database setup

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
 
@@ -10,12 +11,6 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@loc
 # Create async engine
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Create session factory
-AsyncSessionLocal = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Dependency to get DB session
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
+Base = declarative_base()
