1655dcdeb4680503630faa6a0d24cf90d5962e12
Lawrence <claudiuslawrence@gmail.com>
2025-03-18 22:44:20 +0400

    added new loop script to save disease info

---
+++ app/scripts/diseaseFetchDB.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends
+from app.core.database import get_db
+from typing import List, Dict
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from txgnn import TxData, TxGNN, TxEval
+import json
+from app.schemas.schemas import *
+from sqlalchemy.orm import Session
+from app.models.models import *
+from app.services.crud import save_txgnn
+from sqlalchemy.ext.asyncio import AsyncSession
+import csv
+from txgnn_query import get_node_id_by_name, get_drug_id
+
+CSV_FILE_PATH = "/home/dgx/dgx_irkg_be/TxGNN/data/filtered_disease_data.csv "
+_range = 200
+
+TxD = TxData(data_folder_path='/home/dgx/dgx_irkg_be/TxGNN/data')
+TxD.prepare_split(split='complex_disease', seed=42)
+TxG = TxGNN(data=TxD, 
+                weight_bias_track=False,
+                proj_name='TxGNN',
+                exp_name='TxGNN',
+                device='cpu'
+                )
+TxG.load_pretrained_graphmask('/home/dgx/dgx_irkg_be/TxGNN/data/rare_disease_model_ckpt')
+TxE = TxEval(model=TxG)
+
+def txgnn_get(disease) -> DiseaseResponse:
+    disease_idx = get_node_id_by_name(disease)
+    results = TxE.eval_disease_centric(disease_idxs=disease_idx, 
+                                relation='indication',
+                                show_plot=False, 
+                                verbose=False,
+                                save_result=False,
+                                return_raw=False,
+                                # save_name=save_path
+                                )
+
+    limited_result = results.iloc[0]['Prediction'].copy()
+    ranked_result = results.iloc[0]['Ranked List'].copy()
+    max_score = max(limited_result.items(), key=lambda x: x[1])[1]
+    min_score = min(limited_result.items(), key=lambda x: x[1])[1]
+    threshold = 47
+    normalized_predictions = {
+        drug: (float(score) - float(min_score)) / (float(max_score) - float(min_score))
+        for drug, score in limited_result.items()
+    }
+
+    ranked_range = ranked_result[:_range]
+
+    drugs_info = []
+    for drug in ranked_range:
+        score = normalized_predictions.get(get_drug_id(drug), None)
+        # Append drug with the rank and score
+        drugs_info.append(DrugInfo(drug=drug, score=score, rank=0))  # Temporary 'None' for rank
+
+    # Sort drugs by score in descending order and assign ranks
+    sorted_drugs_info = sorted(drugs_info, key=lambda x: x.score, reverse=True)
+    for i, drug in enumerate(sorted_drugs_info):
+        drug.rank = i + 1  # Rank starts from 1
+
+    # Prepare the response object with the disease name and the list of drugs with scores and ranks
+    response = DiseaseResponse(
+        disease_name=results.iloc[0]['Name'] if isinstance(results.iloc[0]['Name'], list) else results.iloc[0]['Name'],
+        drugs=sorted_drugs_info
+    )
+    return response
+
+def main(db: AsyncSession = Depends(get_db)):
+    with open(CSV_FILE_PATH, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)  # Read as a dictionary
+        for row in reader:
+            results = txgnn_get(row['node_name'])
+            save_txgnn(db, results)
+    print('Done')
+
+# Run the script
+if __name__ == "__main__":
+    main()
